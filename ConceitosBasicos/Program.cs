de1b194088deb788e1f91a948514696bc40b018a
HannahFerreira <202311070@unifoa.edu.br>
2025-08-13 21:09:30 -0300

    EXE 1 iRPF

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -1,7 +1,9 @@
-﻿byte idade;
-string nome;
-Console.Write("Digite seu nome: ");
-nome = Console.ReadLine();
-Console.Write("Digite a sua idade: ");
-idade = Convert.ToByte(Console.ReadLine());
-Console.WriteLine("Olá " + nome + " A idade informada Foi {0}", idade );
+﻿
+// ALTERE O PROGRAMA PARA QUE SOLICITE O USUARIO INFORMAR O SEU SALARIO E AO FINAL MOSTRAR O DESCONTO O IRPF DO SALARIO FORMADO USE A TABELA DE DESCONTO
+
+double salario;
+Console.Write("Digite seu Salario: ");
+salario = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Desconto IRPF: " + (salario * 0.15));
+
+
